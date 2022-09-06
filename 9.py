8da6159833ed9bd5e07555383d8cf3cc52bbd098
codrutalugoj <codrutalugoj@gmail.com>
2022-09-06 19:29:21 +0200

    palindrome int

---
+++ 9.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
+
+sol = Solution()
+print(sol.isPalindrome(-121))
