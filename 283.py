16ab6dcb761be8207a548643aec092c254a8d518
codrutalugoj <codrutalugoj@gmail.com>
2022-11-19 17:30:00 +0100

    283 - better sol with 2 pointers

--- 283.py
+++ 283.py
@@ -4,13 +4,17 @@ class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        Main idea: Search for non-zero elems with the right pointer and swap that elem with a zero elem 
+        that the left pointer is pointed to.
         """
-        zeros = []
-        while nums.count(0) != 0:
-            nums.remove(0)
-            zeros.append(0)
+        left = 0
 
-        nums.extend(zeros)
+        for right in range(len(nums)):
+            if nums[right] != 0 and nums[left] == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+
+            if nums[left] !=0:
+                left += 1
         print(nums)
 
 sol = Solution()
